f53b4f5e2c849206ac24e91cc0afb29893c90aba
Abdul Quadri <abdulquadri.aq@gmail.com>
2025-12-08 21:06:02 +0100

    push to to get code suggestion

---
+++ app/Services/OnboardingProcessService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+class OnboardingProcessService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
